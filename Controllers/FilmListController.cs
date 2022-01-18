2c2e24120aade2704be5aff05c9527d1292020a6
Vitor <costec222@gmail.com>
2022-01-18 20:55:08 -0300

    update: banco de dados adicionado. Tabela Filmes Adicionada

--- Controllers/FilmListController.cs
+++ Controllers/FilmListController.cs
@@ -1,16 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeuCinema.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeuCinema.Controllers
 {
     public class FilmListController : Controller
     {
-        public IActionResult Index()
+        private readonly DBCinemaContext _context;
+        public FilmListController(DBCinemaContext context)
         {
-            return View();
+            _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Filmes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var filmesModel = await _context.Filmes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (filmesModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(filmesModel);
+        }
+
         public IActionResult Sections()
         {
             return View();
+        }
+
+        private bool FilmesModelExists(int id)
+        {
+            return _context.Filmes.Any(e => e.Id == id);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using MeuCinema.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//builder.Services.AddScoped<Filmes>();
+
+builder.Services.AddDbContext<DBCinemaContext>(options =>
+options.UseSqlServer("Data Source = DESKTOP-O1J22ES\\SQLEXPRESS; Initial Catalog = DBCinema; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"));
 
 var app = builder.Build();
 
