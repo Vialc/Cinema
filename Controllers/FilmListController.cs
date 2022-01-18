88730f59d490f42fc7303b78f2bb060432bca0f6
Vitor <costec222@gmail.com>
2022-01-17 23:28:25 -0300

    v1

---
+++ Controllers/FilmListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuCinema.Controllers
+{
+    public class FilmListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sections()
+        {
+            return View();
+        }
+    }
+}
